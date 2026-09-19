3afa5785a397bc214aa1f247992050c96501376a
kr0ma-git <w4ynekousuke@gmail.com>
2026-09-19 18:35:59 +0800

    chore: Removal of Migrate() automation to speed up supabase connection

--- apps/api/CampusGo.Web/Program.cs
+++ apps/api/CampusGo.Web/Program.cs
@@ -143,11 +143,11 @@ else
     });
 }
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
-}
+// using (var scope = app.Services.CreateScope())
+// {
+//     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+//     db.Database.Migrate();
+// }
 
 app.UseHttpsRedirection();
 
